d3b760787880ed09064eb8e7798895d43f7326e1
GLN Admin <admin@glowlikenothing.com>
2026-06-18 11:31:28 +0600

    Upgrade typography system: use Inter for clean sans-serif layout and set .font-editorial serif headings to professional semibold weight

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap" rel="stylesheet" />
       </Head>
       <body className="antialiased">
         <Main />
@@ -15,3 +15,4 @@ export default function Document() {
     </Html>
   );
 }
+
